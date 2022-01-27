6a2cf79269df4d38a3c41af909ca1caf3ddcc8ad
Densv03 <den34092den@gmail.com>
2022-01-28 00:38:35 +0200

    feat(jshint) : add jshint

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const camelCase = link => {
             .split('-')
             .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1))
             .join('');
-    }
+    };
     link = link.split(/\//g).slice(1);
     link.map((el, index, arr) => {
         if(el) {
@@ -25,9 +25,9 @@ const camelCase = link => {
         }
     });
     return link;
-}
+};
 
-console.log(camelCase('asdsad-asdasd/asdasda-asd/asdasd-adsasdasd/'))
+console.log(camelCase('asdsad-asdasd/asdasda-asd/asdasd-adsasdasd/'));
 
 console.log(camelCase(link));
 
