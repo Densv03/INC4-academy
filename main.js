0155f1d84f390ea260be1c55d76ec3ce126cb3e7
Densv03 <den34092den@gmail.com>
2022-01-29 13:52:13 +0200

    feat <main.js> : add methods of str

--- main.js
+++ main.js
@@ -1,36 +1,62 @@
-const url = new URL('https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/');
+// const url = new URL('https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/');
 
 
-let newStr;
-function showHost(obj) {
-    console.log(obj.host);
-}
-function splitStr ( str ) {
-    newStr = str.pathname.split('-');
-}
+// let newStr;
+// function showHost(obj) {
+//     console.log(obj.host);
+// }
+// function splitStr ( str ) {
+//     newStr = str.pathname.split('-');
+// }
 
 
-function strToCamelCase( str) {
-     const res = str.map( item => {
-         if( item[0] === '/') {
-             return item[1].toUpperCase() + item.substring(2);
-         } else{
-            return item[0].toUpperCase() + item.substring(1);
-         }
-     });
-     console.log(res.join(''));
-}
+// function strToCamelCase( str) {
+//      const res = str.map( item => {
+//          if( item[0] === '/') {
+//              return item[1].toUpperCase() + item.substring(2);
+//          } else{
+//             return item[0].toUpperCase() + item.substring(1);
+//          }
+//      });
+//      console.log(res.join(''));
+// }
 
-showHost(url);
-splitStr(url);
-strToCamelCase(newStr);
-
-
-
+// showHost(url);
+// splitStr(url);
+// strToCamelCase(newStr);
 
 
 
 
 
 
-// Есть строка ‘’https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/' - вытянуть из этой строки домен, вытянуть название статьи, убрать дефис и каждое слово сделать с заглавной буквы
+// const str = '   what-can-Cedefi-bring-to-The-crypto-Industry/';
+
+// //Длинна строки
+// console.log(str.length);
+// //Обрезаем пробелы
+// console.log(str.trim());
+// //Заканчивается ли строка символом "/"
+// console.log(str.endsWith('/'));
+// //Ищем индекс заданного елемента
+// console.log(str.lastIndexOf('-'));
+// //Содержит ли строка определенный елемент или набор смиволов, слово
+// console.log(str.includes('defi'));
+// //В верхний регистр
+// console.log(str.toUpperCase());
+// //В нижний регистр
+// console.log(str.toLowerCase());
+// //Возвращаем значение строки
+// console.log(str.valueOf());
+// //Разбивает строку на массив значений по заданному параметру
+// console.log(str.split('-'));
+// //Возвращает новую строку с извлечением значений начиная с начально заданного
+// //индекса и заканчивая конечно заданным индексом
+// console.log(str.slice(14,18));
+// //Возвращение формы нормализации Юникода данной строки
+// console.log(str.normalize('NFC'));
+// // Возвращает новую строку с извлечением значений начиная с начально заданного
+// // индекса и заканчивая конечно заданным индексом
+// console.log(str.substring(14,18));
+// //Возвращает новую строку, содержащую указанное количество соединённых  копий строки
+// console.log(str.repeat(2));
