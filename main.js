61a88422dcf14caa7fc8776964af9cf9041db771
Densv03 <den34092den@gmail.com>
2022-01-29 17:27:57 +0200

    feat(main.js) : add methods of number

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// const url = new URL('https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/');
-//
-// function getHost(obj) {
-//     return obj.host;
-// }
-//
-// function strToCamelCase(str) {
-//      const res = str.pathname.split('-').map( item => {
-//          if( item[0] === '/') {
-//              return item[1].toUpperCase() + item.substring(2);
-//          } else{
-//             return item[0].toUpperCase() + item.substring(1);
-//          }
-//      });
-//      return res.join('');
-// }
-//
-// console.log(getHost(url));
-// console.log(strToCamelCase(url));
+const url = new URL('https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/');
+
+function getHost(obj) {
+    return obj.host;
+}
+
+function strToCamelCase(str) {
+     const res = str.pathname.split('-').map( item => {
+         if( item[0] === '/') {
+             return item[1].toUpperCase() + item.substring(2);
+         } else{
+            return item[0].toUpperCase() + item.substring(1);
+         }
+     });
+     return res.join('');
+}
+
+console.log(getHost(url));
+console.log(strToCamelCase(url));
 
 
 
@@ -53,3 +53,24 @@
 // console.log(str.substring(14,18));
 // //Возвращает новую строку, содержащую указанное количество соединённых  копий строки
 // console.log(str.repeat(2));
+
+// const number = 1372.357;
+
+// //Обрезаем количество цифр после запятой
+// console.log(number.toFixed(1));
+// //Превращает число в строку
+// console.log( number.toString());
+// //Возвращает значение числа
+// console.log(number.valueOf());
+// //Определяет является ли числ целым
+// console.log(Number.isInteger(number));
+// //Определяет, является ли переданное значение конечным числом.
+// console.log(Number.isFinite(number));
+// //Возвращает строку, представляющую объект Number в экспоненциальной записи.
+// console.log(number.toExponential());
+// //Возвращает число с заданной точностью
+// console.log(number.toPrecision(5));
+// //Проверяет является ли значение NaN
+// console.log(Number.isNaN(number));
+// //Определяет является ли заданное число безопасным целым числом
+// console.log(Number.isSafeInteger(number));
